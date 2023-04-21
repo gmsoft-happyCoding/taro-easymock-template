350412075498b83b2f1a3beb76ebb9318caa93a7
unknown <359206421@qq.com>
2023-04-21 17:42:51 +0800

    修改类型错误

--- template/common/type.ts
+++ template/common/type.ts
@@ -16,7 +16,7 @@ export type Opts = Taro.request.Option & Extend;
 export type WithPathOpts = Opts & PathParam;
 
 export type Conf = {
-  method: Taro.request.Method;
+  method: string;
   url: string;
   opts?: Opts | WithPathOpts;
 };
